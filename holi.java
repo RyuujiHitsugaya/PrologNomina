965ea597754a18305cabe7b597b3307712e2105a
Brallan Mendoza <keidragon14@gmail.com>
2017-05-22 09:32:19 -0500

    Update holi.java

--- holi.java
+++ holi.java
@@ -3,7 +3,7 @@ public class PruebaIterador{
     Espacio[] space=new Espacio[10];
     for(int i=0;i<space.length();i++)
       space[i]=new Espacio("ocupado");
-    IteradorEspacios iterador=Espacio.iterador();
+    IteradorEspacios iterador=Espacio.iterador(space);
     while(iterador.mirarSiguiemte()){
       Espacio lugarActual=iterador.siguiente();
       System.out.println("Espacio "+lugarActual.estado()); 
